9fc919306c7565f4fa17673bb2a001bb0530bc80
Erik Hansson <hansson_erik@outlook.com>
2020-11-07 21:59:19 +0100

    refact: add Home component

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,3 +1,13 @@
 import { ReactElement } from 'react'
+import Head from 'next/head'
+import { View } from './Home.View'
 
-export const Home = (): ReactElement => <div>About</div>
+export const Home = (): ReactElement => (
+  <>
+    <Head>
+      <title>About Erik Hansson</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <View />
+  </>
+)
